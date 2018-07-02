eeea31ae79b5dafd3583f41aaffb93c1d20d682d
Asley Tan Weihau <36515847+sniffymon@users.noreply.github.com>
2018-07-02 20:53:53 +0800

    added dependencies

--- index.js
+++ index.js
@@ -160,6 +160,70 @@ From 1 out of 10! Which would you like?!
             message.channel.send("No such command exists.");
 
     }
+
+    //MUSIC FUNCTIONS
+
+    async function handleVideo(video, msg, voiceChannel, playlist = false) {
+        const serverQueue = queue.get(msg.guild.id);
+        console.log(video);
+        const song = {
+            id: video.id,
+            title: Util.escapeMarkdown(video.title),
+            url: `https://www.youtube.com/watch?v=${video.id}`
+        };
+        if (!serverQueue) {
+            const queueConstruct = {
+                textChannel: msg.channel,
+                voiceChannel: voiceChannel,
+                connection: null,
+                songs: [],
+                volume: 5,
+                playing: true
+            };
+            queue.set(msg.guild.id, queueConstruct);
+    
+            queueConstruct.songs.push(song);
+    
+            try {
+                var connection = await voiceChannel.join();
+                queueConstruct.connection = connection;
+                play(msg.guild, queueConstruct.songs[0]);
+            } catch (error) {
+                console.error(`I could not join the voice channel: ${error}`);
+                queue.delete(msg.guild.id);
+                return msg.channel.send(`I could not join the voice channel: ${error}`);
+            }
+        } else {
+            serverQueue.songs.push(song);
+            console.log(serverQueue.songs);
+            if (playlist) return undefined;
+            else return msg.channel.send(`I have added the song **${song.title}** to the queue!`);
+        }
+        return undefined;
+    }
+    
+    function play(guild, song) {
+        const serverQueue = queue.get(guild.id);
+    
+        if (!song) {
+            serverQueue.voiceChannel.leave();
+            queue.delete(guild.id);
+            return;
+        }
+        console.log(serverQueue.songs);
+    
+        const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+            .on('end', reason => {
+                if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+                else console.log(reason);
+                serverQueue.songs.shift();
+                play(guild, serverQueue.songs[0]);
+            })
+            .on('error', error => console.error(error));
+        dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    
+        serverQueue.textChannel.send(`🎶 I'll start singing: **${song.title}**`);
+    }
 });
 
 client.login(process.env.TOKEN);

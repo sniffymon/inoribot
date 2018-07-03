3832671d10743eaca1f55cb073919a5e280b6073
Asley Tan Weihau <36515847+sniffymon@users.noreply.github.com>
2018-07-04 02:59:36 +0800

    remake purge

--- index.js
+++ index.js
@@ -136,53 +136,25 @@ client.on("message", async message => {
         break;
         //PURGE
         case "purge":
-<<<<<<< HEAD
-          /*  var user = message.mentions.users.first();
-            const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-=======
             var user = message.mentions.users.first();
->>>>>>> parent of 35ec860... Update Math
             if (!message.member.roles.find("name", adminrolename)) {
                 message.channel.send('You do not have the permissions for that command!');
                 return;
             }
             
-        if (!amount) return message.reply('Please insert the number of messages you\'d like to purge. \n Usage: ' + PREFIX + 'purge <amount> (user)');
-        if (!amount && !user) return message.reply('Please insert the number of messages you\'d like to purge. \n Usage: ' + PREFIX + 'purge <amount> (user)');
+        if (!args[1]) return message.reply('Please insert the number of messages you\'d like to purge. \n Usage: ' + PREFIX + 'purge <amount>');
+        if (!args[1] && !user) return message.reply('Please insert the number of messages you\'d like to purge. \n Usage: ' + PREFIX + 'purge <amount>');
             message.channel.fetchMessages({
-              limit: amount,
+              limit: args[1],
             }).then((messages) => {
             if (user) {
             const filterBy = user ? user.id : Client.user.id;
-<<<<<<< HEAD
-            messages = messages.filter(m => m.author.id === filterBy).array().slice(0, parseInt(amount));
+            messages = messages.filter(m => m.author.id === filterBy).array().slice(0, parseInt(args[1]) + 2);
             }
         
         message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-        message.reply("I removed " + amount + " messages!").then(d_msg => {d_msg.delete(5000); });
-        });*/
-
-        const user = message.mentions.users.first();
-const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-if (!amount) return message.reply('Must specify an amount to delete!');
-if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
-message.channel.fetchMessages({
- limit: amount,
-}).then((messages) => {
- if (user) {
- const filterBy = user ? user.id : Client.user.id;
- messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
- }
- message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-});
-=======
-            messages = messages.filter(m => m.author.id === filterBy).array().slice(0, parseInt(args[1]) + 1);
-            }
-        
-        message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-        message.reply(args[1] + " messages removed!").then(d_msg => {d_msg.delete(5000); });
+        message.reply(", I removed " + args[1] + " messages!").then(d_msg => {d_msg.delete(5000); });
         });
->>>>>>> parent of 35ec860... Update Math
 
         break;
         //MUSIC PLAY

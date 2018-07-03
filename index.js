0081866c45fde106a62eeb072b2f4eaea14223e3
Asley Tan Weihau <36515847+sniffymon@users.noreply.github.com>
2018-07-04 02:47:23 +0800

    outsource code snippet test

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ client.on("message", async message => {
         break;
         //PURGE
         case "purge":
-            var user = message.mentions.users.first();
+          /*  var user = message.mentions.users.first();
             const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
             if (!message.member.roles.find("name", adminrolename)) {
                 message.channel.send('You do not have the permissions for that command!');
@@ -155,7 +155,21 @@ client.on("message", async message => {
         
         message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
         message.reply("I removed " + amount + " messages!").then(d_msg => {d_msg.delete(5000); });
-        });
+        });*/
+
+        const user = message.mentions.users.first();
+const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
+if (!amount) return message.reply('Must specify an amount to delete!');
+if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
+message.channel.fetchMessages({
+ limit: amount,
+}).then((messages) => {
+ if (user) {
+ const filterBy = user ? user.id : Client.user.id;
+ messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+ }
+ message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+});
 
         break;
         //MUSIC PLAY

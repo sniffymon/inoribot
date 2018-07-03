9e65cc27431576735fff5ddebb33cdfbfbdc4f01
Asley Tan Weihau <36515847+sniffymon@users.noreply.github.com>
2018-07-04 01:28:20 +0800

    Added Purge Embedding

--- index.js
+++ index.js
@@ -146,10 +146,16 @@ client.on("message", async message => {
             }).then((messages) => {
             if (user) {
             const filterBy = user ? user.id : Client.user.id;
-            messages = messages.filter(m => m.author.id === filterBy).array().slice(0, args[1]);
+            messages = messages.filter(m => m.author.id === filterBy).array().slice(0, args[1] + 1);
             }
         message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-        message.delete();
+        const purgeembed = new Discord.RichEmbed()
+                        .setColor(0xff7ff0)
+                        .setTitle("Purged")
+                        .setAuthor("Inori")
+                        .setDescripttion( args[1] + 1 + "messages removed.")
+                    message.channel.send(purgeembed);
+        setTimeout(function() { message.delete()}, 5000);
         });
 
         break;

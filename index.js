538b3aed3dfcc72e523422896fa4c3d808815d63
Asley Tan Weihau <36515847+sniffymon@users.noreply.github.com>
2018-07-06 22:09:19 +0800

    Revert "Added inactivenull"
    This reverts commit 41348a1f71146390e50de1f34312ea3e2b222470.

--- index.js
+++ index.js
@@ -365,15 +365,14 @@ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
         if (!song) {
             console.log("Bot disconecting in 1 minute");
             message.channel.send("The song has ended! If there's nothing else, I'll be leaving in a minute!")
-            inactivetimer = setTimeout(function() {message.member.voiceChannel.leave()}, 60000);
+            inactivetimer = setTimeout( function() {message.member.voiceChannel.leave()}, 60000);
 
             queue.delete(guild.id);
          
             return;
         }
         else {
-                clearTimeout(inactivetimer);
-                inactivetimer = null;
+                 clearTimeout(inactivetimer);
                 console.log("A timeout timer has been reset!")
             
             return;
